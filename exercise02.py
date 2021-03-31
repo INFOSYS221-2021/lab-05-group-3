13af05beaf41c3251f0a250b76d0f6c1f21d05ba
Tenatic-X <79816687+Tenatic-X@users.noreply.github.com>
2021-03-31 14:47:37 +1300

    fibonacci update

--- exercise02.py
+++ exercise02.py
@@ -1,4 +1,13 @@
 # Exercise Two
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
 
+def fibonacciPos(position): 
+    if position == 1:
+        return 0;
+    elif position == 2:
+        return 1;      
+    else:
+        return fibonacciPos(position-1)+fibonacciPos(position-2)
+
+
 print("The value at the 6th postion in the Fibonacci sequence is 8")
