6e3320c0efec984d3fe3b6e960fd9f071c90d1c9
Tenatic-X <79816687+Tenatic-X@users.noreply.github.com>
2021-03-31 14:48:45 +1300

    digit length update

--- exercise03.py
+++ exercise03.py
@@ -1,4 +1,9 @@
 # Exercise Three
 # Write a simple program that finds the number of digits of a given integer value
+def number_of_digits(n):
+    if n < 0: 
+        return len(str(n)) - 1
+    else:
+        return len(str(n))
 
 print("The number of digits for a value of 100 is 3")
