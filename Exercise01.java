e59205ce667a6f882bb3ca95348b0a7177159b90
Tenatic-X <79816687+Tenatic-X@users.noreply.github.com>
2021-03-31 14:57:29 +1300

    compare palindrome update

--- Exercise01.java
+++ Exercise01.java
@@ -2,14 +2,14 @@ public class ExerciseOne {
     public static void main(String args[]) {
       ExerciseOne ex01 = new ExerciseOne();
 
-      System.out.println(ex01.reverseString("hello world"));
-      System.out.println(ex01.reverseString("Dad"));
-      System.out.println(ex01.reverseString("Bsuiness School"));
+      //System.out.println(ex01.reverseString("hello world"));
+      //System.out.println(ex01.reverseString("racecar"));
+      //System.out.println(ex01.isStringPalindrome("racecar"));
       
-      // Uncomment the following statements after finishing Exercise Four
-    //   System.out.println(ex01.isStringPalindrome("hello world"));
-    //   System.out.println(ex01.isStringPalindrome("Dad"));
-    //   System.out.println(ex01.isStringPalindrome("radar"));
+      //Uncomment the following statements after finishing Exercise Four
+    System.out.println(ex01.isStringPalindrome("hello world"));
+    System.out.println(ex01.isStringPalindrome("Dad Dad"));
+    System.out.println(ex01.isStringPalindrome("radar"));
     }
     
     // Exercise One
@@ -25,6 +25,6 @@ public class ExerciseOne {
     // Exercise Four
     private boolean isStringPalindrome(String word) {
         // Complete this method to deermine if the given word is a palindrome
-        return false;
+        return (word.toLowerCase()).equals(reverseString(word.toLowerCase()));
     }
 }
